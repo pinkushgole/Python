b49db577ce047631c9d25db071d02397bcd182d4
pinkushgole <pinkushgole@gmail.com>
2024-08-09 17:21:10 +0530

    break and continue

---
+++ Breake_Con.py
@@ -0,0 +1,12 @@
+# for i in range(1,15):
+#     if(i==13):
+#         break
+#     else:
+#         print(i)
+
+
+for i in range(1,15):
+    if(i==13):
+        continue
+    else:
+        print(i)
