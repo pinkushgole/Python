edf2f0b9a04170e2aedc8c6c87fa3428d6f4d62e
pinkushgole <pinkushgole@gmail.com>
2024-08-13 13:36:00 +0530

    class type variable ans instance type variable

---
+++ Instance_variable_Or_Class_Variable.py
@@ -0,0 +1,12 @@
+class Student:
+    Collage_Name="Davv,indore"   
+    def __init__(self,name="Abc",roll=12):
+        self._name=name
+        self._roll=roll
+    
+    def __str__(self):
+        return f"name : {self._name} ,Collage Name :{self.Collage_Name} , roll No : {self._roll}"
+ 
+st=Student("hitesh",1)
+
+print(st) 
