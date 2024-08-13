93c9fd6d880734f75c83a5b540a4cb8977b22554
pinkushgole <pinkushgole@gmail.com>
2024-08-13 15:43:44 +0530

    Update dir_dict_help.py

---
+++ dir_dict_help.py
@@ -0,0 +1,14 @@
+# x=[12,3,4,5,1]
+# print(dir(x))
+# print(x.__add__)
+# print(x.pop())
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+p=Person("Raja",12)
+print(p.__dict__)
+
+print(help(Person))
